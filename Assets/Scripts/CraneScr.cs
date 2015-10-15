5f3867d2d024d0879c255d9e865f6352e376a871
PeachTreeOath <peachtreeoath@gmail.com>
2015-10-14 23:19:20 -0600

    basic stacking implementation

--- Assets/Scripts/CraneScr.cs
+++ Assets/Scripts/CraneScr.cs
@@ -1,37 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class CraneScr : MonoBehaviour {
+public class CraneScr : MonoBehaviour
+{
 
 	public int boundL;
 	public int boundR;
 	public float speed;
-
+	public float generationTime;
+	public GameObject animalPF1;
+	public GameObject animalPF2;
+	public GameObject animalPF3;
+	public GameObject animalPF4;
 	private Rigidbody2D body;
 	private GameObject pile;
+	private List<GameObject> animalPFs;
+	private float currSpeed;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		currSpeed = speed;
 		body = GetComponent<Rigidbody2D> ();
-		body.velocity = new Vector2 (speed, 0);
+		body.velocity = new Vector2 (currSpeed, 0);
 
 		pile = GameObject.Find ("Pile");
+		animalPFs = new List<GameObject> ();
+		animalPFs.Add (animalPF1);
+		animalPFs.Add (animalPF2);
+		animalPFs.Add (animalPF3);
+		animalPFs.Add (animalPF4);
+
+		GenerateChild ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown ("Jump")) {
-			GameObject animalObj = GameObject.Find("DonkeyPF");
-			Rigidbody2D animalBody = animalObj.GetComponent<Rigidbody2D>();
+	void Update ()
+	{
+		GameObject payload = GameObject.FindGameObjectWithTag ("Undropped");
+		if (Input.GetButtonDown ("Jump") && payload != null) {
+			Rigidbody2D animalBody = payload.GetComponent<Rigidbody2D> ();
 			animalBody.isKinematic = false;
-			animalObj.transform.parent = pile.transform;
+			payload.transform.parent = pile.transform;
+			payload.tag = "Dropped";
+
+			Invoke ("GenerateChild", generationTime);
 		}
 
-		if (transform.position.x > boundR || transform.position.x < boundL) {
-			speed = -speed;
-
+		if (transform.position.x > boundR) {
+			currSpeed = -speed;
+		} else if (transform.position.x < boundL) {
+			currSpeed = speed;
 		}
-		body.velocity = new Vector2 (speed, 0);
+		body.velocity = new Vector2 (currSpeed, 0);
 	}
 
+	private void GenerateChild ()
+	{
+		int index = Random.Range (0, 4);
+		Debug.Log (index);
+		GameObject pf = animalPFs [index];
+		GameObject newObj = (GameObject)Instantiate (pf, new Vector3 (0, 0, 0), Quaternion.identity);
+		Transform newTransform = newObj.GetComponent<Transform> ();
+		newTransform.parent = transform;
+		newTransform.localPosition = new Vector2 (0, 0);
+	}
 }
